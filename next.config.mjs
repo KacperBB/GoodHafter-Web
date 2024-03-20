1894fedb97abd40db8719f188f4c94aa3958f50f
KacperBB <kacper.borowiec1999@gmail.com>
2024-03-20 21:11:17 +0100

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,30 @@
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+    process.env.LD_LIBRARY_PATH == null ||
+    !process.env.LD_LIBRARY_PATH.includes(
+        `${process.env.PWD}/node_modules/canvas/build/Release:`,
+    )
+) {
+    process.env.LD_LIBRARY_PATH = `${
+        process.env.PWD
+    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        config.externals = [
-            ...config.externals,
-            ({ context, request }, callback) => {
-                if (request === 'canvas') {
-                    return callback(null, 'commonjs canvas');
-                }
-                callback();
-            },
-        ];
+        webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+                config.externals = [
+                        ...config.externals,
+                        ({ context, request }, callback) => {
+                                if (request === 'canvas') {
+                                        return callback(null, 'commonjs canvas');
+                                }
+                                callback();
+                        },
+                ];
 
-        return config;
-    },
+                return config;
+        },
 };
 
 export default nextConfig;
